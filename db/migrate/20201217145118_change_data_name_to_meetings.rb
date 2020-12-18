b55b2613f73d4a742457e178367802749494b57f
Gumimal <barbarylion000@gmail.com>
2020-12-18 03:10:47 +0000

    ミーティング変更

--- db/migrate/20201217145118_change_data_name_to_meetings.rb
+++ db/migrate/20201217145118_change_data_name_to_meetings.rb
@@ -1,5 +1,5 @@
 class ChangeDataNameToMeetings < ActiveRecord::Migration[5.0]
   def change
-    change_column :meetings, :name, :integer
+    change_column :meetings, :name, "USING name::integer"
   end
 end
