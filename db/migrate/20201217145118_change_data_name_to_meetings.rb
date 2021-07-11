f284076b34cf523f419b0bee97b0b5ab269e1cfa
barbarylion000 <barbarylion000@gmail.com>
2021-07-11 13:48:22 +0000

    マイグレーションファイル　一部削除

--- db/migrate/20201217145118_change_data_name_to_meetings.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDataNameToMeetings < ActiveRecord::Migration[5.0]
-  def change
-    change_column :meetings, :name, :integer
-  end
-end
--- db/migrate/20210614120209_create_meetings.rb
+++ db/migrate/20210614120209_create_meetings.rb
@@ -1,7 +1,7 @@
 class CreateMeetings < ActiveRecord::Migration[5.0]
   def change
     create_table :meetings do |t|
-      t.string :name
+      t.integer :name
       t.datetime :start_time
       t.integer :fat
 
